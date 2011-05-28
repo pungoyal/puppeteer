f786ae19f7e7f21e8aa675c6942d89c85ce75ac2
Puneet Goyal <pungoyal@gmail.com>
2011-05-28 13:14:25 +1000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puppeteer::Application.config.secret_token = '656531f4022fbbaf0c298d17628b40cf667899ade72463605fe57c2f502fc978ab044bf8abaf6a08c2cd3be3941f8ade5ab508034bd02a2b61a4bf45ea26bc5e'
